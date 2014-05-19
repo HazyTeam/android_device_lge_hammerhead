33b77f8ca6f0bd2566efce214de03ef4825996ae
Francesco Colella <mnemonyc@hotmail.com>
2014-05-19 22:33:07 +0200

    Hazy test

--- hazy_hammerhead.mk
+++ hazy_hammerhead.mk
@@ -39,5 +39,10 @@ PRODUCT_MODEL := Nexus 5
 PRODUCT_MANUFACTURER := LGE
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
+# Kernel inline build
+TARGET_KERNEL_CONFIG := hammerhead_defconfig
+TARGET_VARIANT_CONFIG := hammerhead_defconfig
+TARGET_SELINUX_CONFIG := hammerhead_defconfig
+
 $(call inherit-product, device/lge/hammerhead/device.mk)
 $(call inherit-product-if-exists, vendor/lge/hammerhead/device-vendor.mk)
